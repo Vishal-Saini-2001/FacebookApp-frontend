75314d903c183874f8224cfdf3d0cc62f0e0484c
Vishal Saini <vishalsaini3054@gmail.com>
2024-08-29 12:52:32 +0530

    login btn 2

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,40 +1,26 @@
 import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        FB.login(function(response) {
+        console.log(response);
+        if (response.status === 'connected') {
+             navigate('/loggedin')
+           }
+          });
+    }
+
     useEffect(() => {
-        // const loginBtN = () => {
-        //     (function (d, s, id) {
-        //         var js, fjs = d.getElementsByTagName(s)[0];
-        //         if (d.getElementById(id)) { return; }
-        //         js = d.createElement(s); js.id = id;
-        //         js.src = "https://connect.facebook.net/en_US/sdk.js";
-        //         fjs.parentNode.insertBefore(js, fjs);
-        //     }(document, 'script', 'facebook-jssdk')
-        //     );
-
-
-        //     window.fbAsyncInit = function () {
-        //         FB.init({
-        //             appId: '1946793312448448',
-        //             xfbml: true,
-        //             version: 'v20.0'
-        //         });
-        //         // < !--If you are logged in, automatically get your name and email adress, your public profile information-- >
-        //         FB.login(function (response) {
-        //             if (response.authResponse) {
-        //                 console.log('Welcome! Fetching your information.... ');
-        //                 FB.api('/me', { fields: 'name, email' }, function (response) {
-        //                     alert(response.name, response.email)
-        //                 });
-        //             } else {
-        //                 // < !--If you are not logged in, the login dialog will open for you to login asking for permission to get your public profile and email-- >
-        //                 alert('User cancelled login or did not fully authorize.');
-        //             }
-        //         });
-
-        //     };
-        // }
+        FB.getLoginStatus(function (response) {
+            console.log(response);
+            if (response.status === 'connected') { 
+                // navigate('/loggedin')
+            } 
+        });
 
     }, [])
 
@@ -43,7 +29,7 @@ const Login = () => {
             <h1 className='text-center my-4 fw-bold'>Login Page</h1>
             <br />
             <center>
-                <div class="fb-login-button" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="true"></div>
+                <button className='btn btn-primary p-3 fw-bold fs-4' onClick={handleLoginClick}>Login With Facebook</button>
             </center>
         </div>
     )
